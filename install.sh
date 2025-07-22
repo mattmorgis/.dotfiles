421b684f070bd19c04d537159e19e71e8f1cc378
Matthew Morgis <matthew.morgis@gmail.com>
2025-07-21 21:54:05 -0400

    add aerospace config

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ ln -sf "$DOTFILES_DIR/zsh/zshenv" "$HOME/.zshenv"
 ln -sf "$DOTFILES_DIR/zsh/zprofile" "$HOME/.zprofile" 
 ln -sf "$DOTFILES_DIR/zsh/zshrc" "$HOME/.zshrc"
 ln -sf "$DOTFILES_DIR/zfunc" "$HOME/.zfunc"
+ln -sf "$DOTFILES_DIR/config/aerospace/aerospace.toml" "$HOME/.config/aerospace/aerospace.toml"
 
 echo "Dotfiles installed!"
 echo "Restart your shell or run: exec zsh"
