5ab2889e2bcbd2b1561681810d3eba1c8675e510
Matthew Morgis <matthew.morgis@gmail.com>
2025-07-21 22:10:30 -0400

    add git and starship configs

--- install.sh
+++ install.sh
@@ -4,13 +4,30 @@ DOTFILES_DIR="$HOME/.dotfiles"
 
 echo "Installing dotfiles..."
 
-# Create symlinks
-echo "Creating symlinks..."
+# zsh
 ln -sf "$DOTFILES_DIR/zsh/zshenv" "$HOME/.zshenv"
 ln -sf "$DOTFILES_DIR/zsh/zprofile" "$HOME/.zprofile" 
 ln -sf "$DOTFILES_DIR/zsh/zshrc" "$HOME/.zshrc"
 ln -sf "$DOTFILES_DIR/zfunc" "$HOME/.zfunc"
+
+# starship
+ln -sf "$DOTFILES_DIR/config/starship.toml" "$HOME/.config/starship.toml"
+
+# aerospace
+mkdir -p "$HOME/.config/aerospace"
 ln -sf "$DOTFILES_DIR/config/aerospace/aerospace.toml" "$HOME/.config/aerospace/aerospace.toml"
+
+# git 
+mkdir -p "$HOME/.config/git"
+ln -sf "$DOTFILES_DIR/git/gitconfig" "$HOME/.gitconfig"
+
+if [ -f "$DOTFILES_DIR/git/gitconfig-personal" ]; then
+    ln -sf "$DOTFILES_DIR/git/gitconfig-personal" "$HOME/.config/git/gitconfig-personal"
+fi
+
+if [ -f "$DOTFILES_DIR/git/gitconfig-work" ]; then
+    ln -sf "$DOTFILES_DIR/git/gitconfig-work" "$HOME/.config/git/gitconfig-work"
+fi
 
 echo "Dotfiles installed!"
 echo "Restart your shell or run: exec zsh"
