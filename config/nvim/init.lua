f87d31d7af29f64867c592b077f180518fe84526
Matthew Morgis <matthew.morgis@gmail.com>
2025-12-31 00:39:13 -0500

    add keymaps to nvim config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,7 @@
 -- https://arslan.io/2023/05/10/the-benefits-of-using-a-single-init-lua-vimrc-file/
 
+-- Settings
+
 -- Set <space> as the leader key
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
@@ -20,19 +22,13 @@ vim.o.breakindent = true
 vim.o.signcolumn = 'yes'
 vim.o.inccommand = 'split'
 vim.o.cursorline = true
-vim.o.scrolloff = 10
+vim.opt.colorcolumn = "80"
+vim.o.scrolloff = 8
 vim.o.splitright = true
 vim.o.splitbelow = true
-
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
--- vim.schedule(function()
---   vim.o.clipboard = 'unnamedplus'
--- end)
-
--- vim.o.updatetime = 250
--- vim.o.timeoutlen = 300
+vim.o.termguicolors = true
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
@@ -45,4 +41,82 @@ vim.o.splitbelow = true
 vim.o.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
+if vim.fn.getenv("TERM_PROGRAM") == "ghostty" then
+  vim.opt.title = true
+  vim.opt.titlestring = "nvim: %{expand('%:t')}"
+end
+
+-- Keymaps
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = '[P]roject [V]iewer' })
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = 'move text down' })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = 'move text up' })
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = 'paste without yanking' })
+vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d", { desc = 'delete without yanking' })
+
+-- macOS
+vim.keymap.set('v', '<D-c>', '"+y', { desc = 'copy to system clipboard' })
+vim.keymap.set('n', '<D-c>', '"+y', { desc = 'copy to system clipboard' })
+vim.keymap.set('n', '<D-s>', ':w<CR>', { desc = 'cmd+s to save' })
+
+vim.keymap.set('n', 'j', 'gj', { desc = 'handle wrapped text' })
+vim.keymap.set('n', 'k', 'gk', { desc = 'handle wrapped text' })
+
+vim.keymap.set('i', 'jj', '<ESC>', { desc = 'exit insert on jj' })
+vim.keymap.set('i', 'kk', '<ESC>', { desc = 'exit insert on kk' })
+
+-- find and replace word under cursor
+vim.keymap.set("n", "<leader>f", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Copy current filepath to system clipboard (relative to git root, fallback to absolute path)
+vim.keymap.set('n', '<Leader>cp', function()
+  local git_prefix = vim.fn.system('git rev-parse --show-prefix'):gsub('\n', '')
+  local path
+  if vim.v.shell_error == 0 then
+    path = git_prefix .. vim.fn.expand('%')
+  else
+    path = vim.fn.expand('%:p')
+  end
+  vim.fn.setreg('+', path)
+  print('Copied to clipboard: ' .. path)
+end, { silent = true })
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.hl.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
 
