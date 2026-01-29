ae24d64567b8e91d08305907df1759d14cd12934
Matthew Morgis <matthew.morgis@gmail.com>
2026-01-28 21:14:16 -0500

    update lua lsp

--- config/nvim/lsp/lua_ls.lua
+++ config/nvim/lsp/lua_ls.lua
@@ -6,12 +6,7 @@ return {
     Lua = {
       runtime = { version = 'LuaJIT' },
       diagnostics = { globals = { 'vim' } },
-      -- workspace = {
-      --   library = vim.api.nvim_get_runtime_file("", true),
-      --   checkThirdParty = false,
-      -- },
       telemetry = { enable = false },
-    }
-  }
+    },
+  },
 }
-
